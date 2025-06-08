284d9d2a76c4afd277c3b129aa5e01e400ffed77
Madi <reds1n1gam1@gmail.com>
2025-06-08 19:16:09 +0300

    feat: Adding secondary entry point

---
+++ projects/tpa/utils/src/lib/utils.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-utils',
+  imports: [],
+  template: `
+    <p>
+      utils works!
+    </p>
+  `,
+  styles: ``
+})
+export class UtilsComponent {
+
+}
