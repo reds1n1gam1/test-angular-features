648987336a17b0f76aeb2a64f9f972042b45442d
Madi <reds1n1gam1@gmail.com>
2025-06-08 18:14:16 +0300

    feat: Small refactor updates

--- src/app/Playbook/playbook-routing.module.ts
+++ src/app/Playbook/playbook-routing.module.ts
@@ -2,16 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PlaybookComponent } from './playbook.component';
 
-
 const routes: Routes = [
-  {
-    path: '',
-    component: PlaybookComponent
-  }
+    {
+        path: '',
+        component: PlaybookComponent
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
 export class PlaybookRoutingModule { }
